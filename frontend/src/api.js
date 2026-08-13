7b602b22021dd909f7c324d8492860534921e7fa
Sandeep Kaur <ksidhu3005@gmail.com>
2026-08-13 04:38:03 +0100

    Point frontend to live backend, open CORS temporarily

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://applyflow-nign.onrender.com",
 });
 
 export const listApplications = (status = null, sortBy = "date_applied") =>
